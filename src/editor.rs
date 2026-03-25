6e3a99ffcb75b52f86182edb98b990cd19ef7c55
shabani005 <hasan.shabani2005@gmail.com>
2026-03-25 21:14:12 +0300

    initial commit

---
+++ src/editor.rs
@@ -0,0 +1,128 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum EditorAction {
+    Move(Direction),
+    Insert(char),
+    Backspace,
+    Save,
+    Quit,
+    Unhandled(String),
+}
+
+pub trait LowerInput {
+    fn get_event(&mut self) -> Option<EditorAction>;
+}
+
+pub struct Editor {
+    pub line: usize,
+    pub column: usize,
+    pub content: String,
+    pub running: bool,
+}
+
+impl Editor {
+    pub fn new() -> Self {
+        Self {
+            line: 0,
+            column: 0,
+            content: String::new(),
+            running: true,
+        }
+    }
+
+    pub fn eval(&mut self, action: EditorAction) {
+        match action {
+            EditorAction::Save => {}
+            EditorAction::Unhandled(_) => {}
+            EditorAction::Move(Direction::Up) => {
+                if self.can_move_up() {
+                    self.line -= 1;
+                    self.snap_column();
+                }
+            }
+            EditorAction::Move(Direction::Down) => {
+                if self.can_move_down() {
+                    self.line += 1;
+                    self.snap_column();
+                }
+            }
+            EditorAction::Move(Direction::Right) => {
+                if self.can_move_right() {
+                    self.column += 1;
+                }
+            }
+            EditorAction::Move(Direction::Left) => {
+                if self.can_move_left() {
+                    self.column -= 1;
+                }
+            }
+            EditorAction::Insert(c) => {
+                let idx = self.get_buffer_index();
+                self.content.insert(idx, c);
+                self.column += 1;
+            }
+            EditorAction::Backspace => {
+                if self.can_backspace() {
+                    self.perform_backspace();
+                }
+            }
+            EditorAction::Quit => self.running = false,
+        }
+    }
+
+    pub fn get_current_line(&self) -> Option<&str> {
+        self.content.lines().nth(self.line)
+    }
+
+    fn snap_column(&mut self) {
+        let len = self.get_current_line().map_or(0, |l| l.len());
+        if self.column > len {
+            self.column = len;
+        }
+    }
+
+    fn can_move_up(&self) -> bool {
+        self.line > 0
+    }
+    fn can_move_down(&self) -> bool {
+        self.line < self.content.lines().count().saturating_sub(1)
+    }
+    fn can_move_right(&self) -> bool {
+        self.get_current_line().map_or(0, |l| l.len()) > self.column
+    }
+    fn can_move_left(&self) -> bool {
+        self.column > 0
+    }
+    fn can_backspace(&self) -> bool {
+        self.line > 0 || self.column > 0
+    }
+
+    fn get_buffer_index(&self) -> usize {
+        self.content
+            .lines()
+            .take(self.line)
+            .map(|l| l.len() + 1)
+            .sum::<usize>()
+            + self.column
+    }
+
+    fn perform_backspace(&mut self) {
+        let index = self.get_buffer_index();
+        if index > 0 {
+            self.content.remove(index - 1);
+            if self.column > 0 {
+                self.column -= 1;
+            } else {
+                self.line -= 1;
+                self.snap_column();
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+mod editor;
+
+use std::io::{self, Write, stdout};
+
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    event::{self, Event, KeyCode, KeyModifiers},
+    terminal::{
+        Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
+        enable_raw_mode,
+    },
+};
+
+use editor::{Direction, Editor, EditorAction, LowerInput};
+
+struct TerminalEditor;
+
+impl LowerInput for TerminalEditor {
+    fn get_event(&mut self) -> Option<EditorAction> {
+        if let Ok(Event::Key(key)) = event::read() {
+            return match key.code {
+                KeyCode::Char('q') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(EditorAction::Quit)
+                }
+                KeyCode::Up => Some(EditorAction::Move(Direction::Up)),
+                KeyCode::Down => Some(EditorAction::Move(Direction::Down)),
+                KeyCode::Left => Some(EditorAction::Move(Direction::Left)),
+                KeyCode::Right => Some(EditorAction::Move(Direction::Right)),
+                KeyCode::Backspace => Some(EditorAction::Backspace),
+                KeyCode::Enter => Some(EditorAction::Insert('\n')),
+                KeyCode::Char(c) => Some(EditorAction::Insert(c)),
+                _ => Some(EditorAction::Unhandled(format!("{:?}", key.code))),
+            };
+        }
+        None
+    }
+}
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+    let mut stdout = stdout();
+    stdout.execute(EnterAlternateScreen)?;
+
+    let mut my_editor = Editor::new();
+    let mut keylifter = TerminalEditor;
+
+    while my_editor.running {
+        stdout.queue(cursor::MoveTo(0, 0))?;
+        stdout.queue(Clear(ClearType::All))?;
+
+        print!(
+            "EDITOR | Line: {}, Col: {} | Ctrl+Q to Quit\r\n",
+            my_editor.line, my_editor.column
+        );
+        print!("-------------------------------------------\r\n");
+
+        let lines: Vec<&str> = my_editor.content.lines().collect();
+        let max_lines = lines.len().max(1);
+
+        for i in 0..max_lines {
+            let text = lines.get(i).unwrap_or(&"");
+            print!("{}\r\n", text);
+        }
+
+        stdout.flush()?;
+
+        if let Some(action) = keylifter.get_event() {
+            my_editor.eval(action);
+        }
+    }
+
+    stdout.execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
