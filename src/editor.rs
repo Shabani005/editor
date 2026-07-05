7d8f8c66bff71879237f4c1965f633b893071083
shabani005 <hasan.shabani2005@gmail.com>
2026-07-05 11:32:01 +0300

    using ropes

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,13 @@
+use ropey::Rope;
+use std::fs;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Modifier {
+    Ctrl,
+    Alt,
+    Shift,
+}
+
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Direction {
     Up,
@@ -6,13 +16,13 @@ pub enum Direction {
     Right,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Command {
     Quit,
-    Save,
-    OpenFile,
+    Save(String),
+    OpenFile(String),
     NewBuffer,
-    SwitchBuffer,
+    SwitchBuffer(usize),
     Copy,
     Cut,
     Paste,
@@ -37,9 +47,12 @@ pub enum Macro {
 pub enum EditorAction {
     Move(Direction),
     Insert(char),
+    InsertString(String),
     Backspace,
-    Run(Command),
-    Execute(Macro),
+    Run(Command),   // TODO
+    Execute(Macro), // TODO
+    KeyUp(Modifier),
+    KeyDown(Modifier),
     Unhandled(String),
 }
 
@@ -50,8 +63,14 @@ pub trait LowerInput {
 pub struct Editor {
     pub line: usize,
     pub column: usize,
-    pub content: String,
+    pub contents: Vec<Rope>,
     pub running: bool,
+    pub ctrl_held: bool,
+    pub commands: Vec<char>,
+    pub sel_start: Option<usize>,
+    pub sel_end: Option<usize>,
+    pub clipboard: String,
+    pub current_buffer: usize,
 }
 
 impl Editor {
@@ -59,8 +78,14 @@ impl Editor {
         Self {
             line: 0,
             column: 0,
-            content: String::new(),
+            contents: vec![Rope::new()],
+            commands: Vec::new(),
+            sel_start: None,
+            sel_end: None,
+            clipboard: String::new(),
             running: true,
+            ctrl_held: false,
+            current_buffer: 0,
         }
     }
 
@@ -69,9 +94,29 @@ impl Editor {
         use Direction::*;
         use EditorAction::*;
         use Macro::*;
+        use Modifier::*;
 
         match action {
             Unhandled(_) => {}
+
+            KeyDown(Ctrl) => {
+                self.ctrl_held = true;
+                self.commands.clear();
+            }
+
+            KeyUp(Ctrl) => {
+                self.ctrl_held = false;
+                self.run_command();
+            }
+
+            KeyDown(key) => {}
+            KeyUp(key) => {}
+
+            action if self.ctrl_held => {
+                if let EditorAction::Insert(c) = action {
+                    self.commands.push(c);
+                }
+            }
 
             Move(Up) => {
                 self.move_up();
@@ -88,18 +133,28 @@ impl Editor {
             Insert(c) => {
                 self.insert_char(c);
             }
+            InsertString(s) => {
+                self.insert_string(s);
+            }
             Backspace => {
                 self.perform_backspace();
             }
 
             Run(Quit) => self.running = false,
-            Run(Save) => {}
-            Run(OpenFile) => {}
+
+            Run(Save(path)) => {
+                self.write_content_to_file(path);
+            }
+
+            Run(OpenFile(path)) => {
+                self.set_content_from_file(path);
+            }
+
             Run(NewBuffer) => {}
             Run(Copy) => {}
             Run(Paste) => {}
             Run(Cut) => {}
-            Run(SwitchBuffer) => {}
+            Run(SwitchBuffer(id)) => {}
 
             Execute(SelectChar) => {}
             Execute(SelectLine) => {}
@@ -117,12 +172,12 @@ impl Editor {
         }
     }
 
-    pub fn get_current_line(&self) -> Option<&str> {
-        self.content.lines().nth(self.line)
+    pub fn get_current_line_len(&self) -> usize {
+        self.content_immutable().line(self.line).len_chars()
     }
 
     fn snap_column(&mut self) {
-        let len = self.get_current_line().map_or(0, |l| l.len());
+        let len = self.get_current_line_len();
         if self.column > len {
             self.column = len;
         }
@@ -132,10 +187,10 @@ impl Editor {
         self.line > 0
     }
     fn can_move_down(&self) -> bool {
-        self.line < self.content.lines().count().saturating_sub(1)
+        self.line < self.content_immutable().lines().count().saturating_sub(1)
     }
     fn can_move_right(&self) -> bool {
-        self.get_current_line().map_or(0, |l| l.len()) > self.column
+        self.get_current_line_len() > self.column
     }
     fn can_move_left(&self) -> bool {
         self.column > 0
@@ -145,29 +200,29 @@ impl Editor {
     }
 
     fn get_buffer_index(&self) -> usize {
-        self.content
-            .lines()
-            .take(self.line)
-            .map(|l| l.len() + 1)
-            .sum::<usize>()
-            + self.column
+        self.content_immutable().line_to_char(self.line) + self.column
     }
 
     fn perform_backspace(&mut self) -> bool {
         let index = self.get_buffer_index();
-        if self.can_backspace() {
-            if index > 0 {
-                self.content.remove(index - 1);
-                if self.column > 0 {
-                    self.column -= 1;
-                } else {
-                    self.line -= 1;
-                    self.snap_column();
-                }
-            }
-            return true;
-        } else {
+
+        if !self.can_backspace() {
             return false;
+        } else {
+            if self.can_backspace() {
+                if index > 0 {
+                    self.content().remove((index - 1)..index);
+                    if self.column > 0 {
+                        self.column -= 1;
+                    } else {
+                        self.line -= 1;
+                        self.snap_column();
+                    }
+                }
+                return true;
+            } else {
+                false
+            }
         }
     }
 
@@ -211,7 +266,79 @@ impl Editor {
 
     fn insert_char(&mut self, c: char) {
         let idx = self.get_buffer_index();
-        self.content.insert(idx, c);
-        self.column += 1;
+        self.content().insert_char(idx, c);
+
+        if c == '\n' {
+            self.line += 1;
+            self.column = 0;
+        } else {
+            self.column += 1;
+        }
+    }
+
+    fn insert_string(&mut self, s: String) {
+        let idx = self.get_buffer_index();
+        self.content().insert(idx, s.as_str());
+
+        let newlines = s.chars().filter(|&c| c == '\n').count();
+        if newlines > 0 {
+            self.line += newlines;
+            self.column = s.rfind('\n').map_or(0, |i| s[i + 1..].len());
+        } else {
+            self.column += s.chars().count();
+        }
+    }
+
+    fn set_content_from_file(&mut self, path: String) {
+        *self.content() = Rope::from_str(self.read_file(path.as_str()).as_str());
+    }
+
+    fn write_content_to_file(&mut self, path: String) {
+        self.write_file(path.as_str());
+    }
+
+    fn read_file(&mut self, path: &str) -> String {
+        fs::read_to_string(path).unwrap()
+    }
+
+    fn write_file(&mut self, path: &str) -> () {
+        fs::write(path, self.content().to_string()).unwrap()
+    }
+
+    pub fn content(&mut self) -> &mut Rope {
+        &mut self.contents[self.current_buffer]
+    }
+
+    pub fn content_immutable(&self) -> &Rope {
+        &self.contents[self.current_buffer]
+    }
+
+    fn new_buffer(&mut self) {
+        self.contents.push(Rope::new());
+        self.current_buffer = self.contents.len() - 1;
+        self.line = 0;
+        self.column = 0;
+    }
+
+    fn switch_buffer(&mut self, id: usize) {
+        if id < self.contents.len() {
+            self.current_buffer = id;
+            self.line = 0;
+            self.column = 0;
+        }
+    }
+
+    fn run_command(&mut self) {
+        use Command::*;
+        use EditorAction::*;
+
+        let cmd: String = self.commands.iter().collect();
+
+        match cmd.as_str() {
+            "s" => self.eval(Run(Save("test.txt".to_string()))),
+            "q" => self.eval(Run(Quit)),
+
+            _ => {}
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::{self, Write, stdout};
 
 use crossterm::{
     ExecutableCommand, QueueableCommand, cursor,
-    event::{self, Event, KeyCode, KeyModifiers},
+    event::{self, DisableBracketedPaste, EnableBracketedPaste, Event, KeyCode, KeyModifiers},
     terminal::{
         Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode,
         enable_raw_mode,
@@ -17,11 +17,16 @@ struct TerminalEditor;
 
 impl LowerInput for TerminalEditor {
     fn get_event(&mut self) -> Option<EditorAction> {
-        if let Ok(Event::Key(key)) = event::read() {
-            return match key.code {
-                KeyCode::Char('q') if key.modifiers.contains(KeyModifiers::CONTROL) => {
-                    Some(EditorAction::Run(Command::Quit))
+        match event::read().ok()? {
+            Event::Paste(txt) => Some(EditorAction::InsertString(txt)),
+
+            Event::Key(key) => match key.code {
+                KeyCode::Modifier(event::ModifierKeyCode::LeftControl) => {
+                    Some(EditorAction::KeyDown(editor::Modifier::Ctrl))
                 }
+                // KeyCode::Char('q') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                //     Some(EditorAction::Run(Command::Quit))
+                // }
                 KeyCode::Up => Some(EditorAction::Move(Direction::Up)),
                 KeyCode::Down => Some(EditorAction::Move(Direction::Down)),
                 KeyCode::Left => Some(EditorAction::Move(Direction::Left)),
@@ -30,16 +35,19 @@ impl LowerInput for TerminalEditor {
                 KeyCode::Enter => Some(EditorAction::Insert('\n')),
                 KeyCode::Char(c) => Some(EditorAction::Insert(c)),
                 _ => Some(EditorAction::Unhandled(format!("{:?}", key.code))),
-            };
+            },
+
+            _ => None,
         }
-        None
     }
 }
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     let mut stdout = stdout();
+
     stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(EnableBracketedPaste)?;
 
     let mut my_editor = Editor::new();
     let mut keylifter = TerminalEditor;
@@ -51,16 +59,21 @@ fn main() -> io::Result<()> {
         print!(
             "EDITOR | Line: {}, Col: {} | Ctrl+Q to Quit\r\n",
             my_editor.line, my_editor.column
-        );
-        print!("-------------------------------------------\r\n");
+        ); // Line 1
+        print!("-------------------------------------------\r\n"); // Line 2
 
-        let lines: Vec<&str> = my_editor.content.lines().collect();
-        let max_lines = lines.len().max(1);
+        let line_count = my_editor.content().len_lines().max(1);
 
-        for i in 0..max_lines {
-            let text = lines.get(i).unwrap_or(&"");
+        for i in 0..line_count {
+            let line = my_editor.content().line(i);
+            let text = line.to_string().trim_end_matches('\n').to_string();
             print!("{}\r\n", text);
         }
+
+        stdout.queue(cursor::MoveTo(
+            my_editor.column as u16,
+            my_editor.line as u16 + 2,
+        ))?;
 
         stdout.flush()?;
 
@@ -71,5 +84,6 @@ fn main() -> io::Result<()> {
 
     stdout.execute(LeaveAlternateScreen)?;
     disable_raw_mode()?;
+    stdout.execute(DisableBracketedPaste)?;
     Ok(())
 }
